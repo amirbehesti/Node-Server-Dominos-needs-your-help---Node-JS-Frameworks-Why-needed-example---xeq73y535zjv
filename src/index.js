c40ba5d85ed9111cbe256c93195b74537cd5f3b9
amirbehesti <amirbehesti392@gmail.com>
2022-09-16 19:52:13 +0530

    fix1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,32 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+    const responseData = {
+        phone: '18602100000',
+        email: 'guestcaredominos@jublfood.com'
+      };
+    switch (req.url) {
+        case '/welcome':
+          // welcome page route
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end('Welcome to Dominos!');
+          break;
+        case '/contact':
+          // contact page route
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+        //   const jsonContent = JSON.stringify(responseData);
+          res.end(JSON.stringify(responseData));
+          break;
+    
+        default:
+          // no match found.
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          res.end('404 error');
+          break;
+      }
 }
 
+httpServer.listen(8081,()=>{
+    console.log("Listening to post 8081");
+});
 module.exports = httpServer;
